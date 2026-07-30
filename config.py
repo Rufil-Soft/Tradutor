2899626f12a822dee6faacd9dcf547a29e47880f
Rui Fernandes <111181596+Rufil-Soft@users.noreply.github.com>
2026-07-30 16:11:46 +0100

    Add configuration for families and limits

---
+++ config.py
@@ -0,0 +1,10 @@
+FAMILIAS = {
+    "corleone": "Família Corleone",
+    "gambino": "Família Gambino",
+    "genovese": "Família Genovese",
+    "lucchese": "Família Lucchese",
+    "bonanno": "Família Bonanno"
+}
+LIMITE_SOLDIERS = 20
+CARGOS_ELEGIVEIS = ["Don", "Capo", "Soldier", "Consigliere", "Capodecina", "Assistente"]
+BLOCK_TIMEOUT = 30  # segundos para fechar bloco de tradução
