121324f59905f0c7e7077fd4ec7cc3c1263c0624
Rui Fernandes <111181596+Rufil-Soft@users.noreply.github.com>
2026-07-31 19:56:54 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,9 @@ FAMILIAS = {
     "gambino": "Família Gambino",
     "genovese": "Família Genovese",
     "lucchese": "Família Lucchese",
-    "bonanno": "Família Bonanno"
+    "bonanno": "Família Bonanno",
+    "colombo": "Família Colombo",  
+    
 }
 LIMITE_SOLDIERS = 20
 CARGOS_ELEGIVEIS = ["Don", "Capo", "Soldier", "Consigliere", "Capodecina", "Assistente"]
